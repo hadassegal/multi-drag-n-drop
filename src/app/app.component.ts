ef7dd11f7d3f8dcf5a241d7e041f73fb2491e20d
hsegal <hsegal@outbrain.com>
2019-12-01 18:23:58 +0200

    improve examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-drag-n-drop';
+
+  plants = [
+    {
+      image: 'https://images-na.ssl-images-amazon.com/images/I/31E7o6bV8cL.jpg',
+      text: 'sunflower'
+    },
+    {
+      image: 'https://d384u2mq2suvbq.cloudfront.net/public/spree/products/1578/jumbo/Plumeria-Fragrance-Oil.jpg',
+      text: 'plumeria'
+    },
+    {
+      image: 'https://images.homedepot-static.com/productImages/f947fc1a-1077-41f7-ab4d-48022188b0a0/svn/spring-hill-nurseries-perennials-65357-64_1000.jpg',
+      text: 'hibiscus'
+    },
+    {
+      image: 'http://www.kalliergeia.com/wp-content/uploads/2018/12/1.-Anthurium-andraeanum-Flamingo-Flower-in-Blooming.jpg',
+      text: 'anthurium'
+    }
+  ]
 
   listItems = [
     'https://images2.minutemediacdn.com/image/upload/c_crop,h_1193,w_2121,x_0,y_64/f_auto,q_auto,w_1100/v1565279671/shape/mentalfloss/578211-gettyimages-542930526.jpg',
